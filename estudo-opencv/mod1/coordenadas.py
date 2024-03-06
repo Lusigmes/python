462cbf0e65afde5e5f55c863bb649b26127e53af
Luis <talkme.lusi@gmail.com>
2024-03-06 20:39:20 -0300

    estudo de visao computacional com open cv

---
+++ estudo-opencv/mod1/coordenadas.py
@@ -0,0 +1,68 @@
+""""" #altura, largura e canais de cores da imagem. exibe e clona a imagem aberta, sai com qualquer botao
+# Importação das bibliotecas
+import cv2
+# Leitura da imagem com a função imread()
+imagem = cv2.imread('../entrada.jpg')
+print('Largura em pixels: ', end='')
+print(imagem.shape[1]) #largura da imagem
+print('Altura em pixels: ', end='')
+print(imagem.shape[0]) #altura da imagem
+print('Qtde de canais: ', end='')
+print(imagem.shape[2])
+# Mostra a imagem com a função imshow
+cv2.imshow("Nome da janela", imagem)
+cv2.waitKey(0) #espera pressionar qualquer tecla
+# Salvar a imagem no disco com função imwrite()
+cv2.imwrite("saida.jpg", imagem)
+"""""
+
+
+""""" #canal de cores bgr de cada posição do pixel
+import cv2
+imagem = cv2.imread('saida.jpg')
+(b, g, r) = imagem[0, 0] 
+print('O pixel (0, 0) tem as seguintes cores:')
+print('Vermelho:', r, 'Verde:', g, 'Azul:', b)
+"""""
+
+""""" #muyda todos pixels para azul
+import cv2
+imagem = cv2.imread('saida.jpg')
+
+for y in range(0, imagem.shape[0]):
+    for x in range(0, imagem.shape[1]):
+        imagem[y, x] = (255,0,0)
+        cv2.imwrite("Imagem_modificada.jpg", imagem)
+        # cv2.imshow("Imagem modificada", imagem)
+"""""
+        
+""""" # resta da divisão por 256” para manter o resultado entre 0 e 255.
+import cv2
+imagem = cv2.imread('saida.jpg')
+for y in range(0, imagem.shape[0]): #percorre linhas
+    for x in range(0, imagem.shape[1]): #percorre colunas
+        imagem[y, x] = (x%256,y%256,x%256)
+cv2.imshow("Imagem modificada", imagem)
+cv2.waitKey(0)
+"""""
+
+""""" # valores de linha multiplicado pela coluna (x*y) no componente “G” da tupla que
+# forma a cor de cada pixel e deixamos o componente azul e vermelho zerados.
+import cv2
+imagem = cv2.imread('saida.jpg')
+for y in range(0, imagem.shape[0], 1): #percorre as linhas
+    for x in range(0, imagem.shape[1], 1): #percorre as colunas
+        imagem[y, x] = (0,(x*y)%256,0)
+cv2.imshow("Imagem modificada", imagem)
+cv2.waitKey(0)
+"""""
+
+# salta a cada 10 pixels ao percorrer as linhas e mais 10 pixels ao
+# percorrer as colunas. A cada salto é criado um quadrado roxo de 5x5 pixels. 
+import cv2
+imagem = cv2.imread('saida.jpg')
+for y in range(0, imagem.shape[0], 10): #percorre linhas
+    for x in range(0, imagem.shape[1], 10): #percorre colunas
+        imagem[y:y+5, x: x+5] = (255,0,255)
+cv2.imshow("Imagem modificada", imagem)
+cv2.waitKey(0)
---
+++ estudo-opencv/mod2/main.py
@@ -0,0 +1,71 @@
+""""" # cria varios retangulos coloridos sobre a imagem
+import cv2
+imagem = cv2.imread('../entrada.jpg')
+# imagem[0][0] = (0,255,0)
+# imagem[0][1] = (0,255,0)
+# imagem[0][2] = (0,255,0)
+# imagem[1][0] = (0,255,0)
+# imagem[2][0] = (0,255,0)
+# imagem[2][2] = (0,255,0)
+imagem[30:100 , 70:100] = (0,255,0)
+#Cria um quadrado vermelho
+imagem[100:150, 50:100] = (0, 0, 255)
+#Cria um retangulo amarelo por toda a altura da imagem
+imagem[:, 200:220] = (0, 255, 255)
+#Cria um retangulo verde da linha 150 a 300 nas colunas 250 a
+350
+imagem[150:300, 250:350] = (0, 255, 0)
+#Cria um quadrado ciano da linha 150 a 300 nas colunas 250 a
+350
+imagem[300:400, 50:150] = (255, 255, 0)
+#Cria um quadrado branco
+imagem[250:350, 300:400] = (255, 255, 255)
+#Cria um quadrado preto
+imagem[70:100, 300: 450] = (0, 0, 0)
+cv2.imwrite("alterada.jpg", imagem)
+cv2.imshow("Imagem", imagem)
+cv2.waitKey(0)
+"""""
+
+""""" # desenho de formas geometricas na imagem
+import numpy as np
+import cv2
+imagem = cv2.imread('../entrada.jpg')
+vermelho = (0, 0, 255)
+verde = (0, 255, 0)
+azul = (255, 0, 0)
+
+cv2.line(imagem, (0, 0), (200, 300), verde)
+cv2.line(imagem, (400, 300), (150, 150), vermelho, 5)
+cv2.rectangle(imagem, (100, 100), (240, 240), azul, 10)
+cv2.rectangle(imagem, (300, 150), (325, 225), verde, -1)
+
+(X, Y) = (imagem.shape[1] // 2, imagem.shape[0] // 2)
+
+for raio in range(0, 300, 50):
+    cv2.circle(imagem, (X, Y), raio, vermelho)
+    
+cv2.imwrite("modificada.jpg", imagem)
+cv2.imshow("Desenhando sobre a imagem", imagem)
+cv2.waitKey(0)
+"""""
+
+""""" #escrita em imagem
+import numpy as np
+import cv2
+imagem = cv2.imread('../entrada.jpg')
+fonte = cv2.FONT_HERSHEY_SIMPLEX
+cv2.putText(imagem,'OpenCV',(15,65), fonte,
+2,(255,255,255),2,cv2.LINE_AA)
+cv2.imshow("onça", imagem)
+cv2.waitKey(0)
+"""""
+
+""""" #recortando img
+import cv2
+imagem = cv2.imread('../entrada.jpg')
+recorte = imagem[436:512, 605:739] #linha 436+1 até 512 e coluna 605+1 até 739
+cv2.imwrite("recorte.jpg", recorte) #salva no disco
+cv2.imshow("Recorte da imagem", recorte)
+cv2.waitKey(0)
+"""""
