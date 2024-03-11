2aa02ae8c413c2995a3345fb93bc1ebf7bf7c89e
Luis <talkme.lusi@gmail.com>
2024-03-11 11:24:17 -0300

    estudo de pln - analise de satisfação do cliiente - libs nltk, streamlit

---
+++ estudo-pln/analise.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+nltk.download("vader_lexicon")
+
+# etapa 1 - criando sistema
+#TITULO
+st.write("# Análise de Satisfação do Cliente")
+#INPUT
+user_i = st.text_input("Digite sua satisfação : ")
+
+# etapa 2 - maquina preditiva e pln
+sentimento = SentimentIntensityAnalyzer()
+score = sentimento.polarity_scores(user_i)
+
+# visualização otimizadaa
+if score == 0 :
+    st.write(score)
+    st.write("## Análise neutra.")
+elif score["neg"] != 0:
+    st.write(score)
+    st.write("## Análise negativa.")
+elif score["pos"] != 0:
+    st.write(score)
+    st.write("## Análise positiva.")
+    st.write(score)
+    
+    
+# etapa 3 - uso do sistema
+# streamlit run analise.py
